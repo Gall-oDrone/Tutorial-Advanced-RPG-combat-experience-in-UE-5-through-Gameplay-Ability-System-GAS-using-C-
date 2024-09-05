a2c90d721c787ce1f5b3c4c35f6700381456ac30
Gall-oDrone <piehavok@hotmail.com>
2024-09-05 17:55:53 -0600

    Start Up Data

---
+++ Source/Warrior/Private/DataAssets/StartUpData/DataAsset_StartUpDataBase.cpp
@@ -0,0 +1,34 @@
+// Diego Gallo All Rights Reserved
+
+
+#include "DataAssets/StartUpData/DataAsset_StartUpDataBase.h"
+#include "AbilitySystem/WarriorAbilitySystemComponent.h"
+#include "AbilitySystem/Abilities/WarriorGameplayAbility.h"
+
+void UDataAsset_StartUpDataBase::GiveToAbilitySystemComponent(UWarriorAbilitySystemComponent* InWarriorASCToGive, int32 ApplyLevel)
+{
+	check(InWarriorASCToGive);
+
+	GrantAbilities(ActivateOnGivenAbilities, InWarriorASCToGive, ApplyLevel);
+	GrantAbilities(ReactiveAbilities, InWarriorASCToGive, ApplyLevel);
+
+}
+
+void UDataAsset_StartUpDataBase::GrantAbilities(const TArray<TSubclassOf<UWarriorGameplayAbility>>& InAbilitiesToGive, UWarriorAbilitySystemComponent* InWarriorASCToGive, int32 ApplyLevel)
+{
+	if (InAbilitiesToGive.IsEmpty())
+	{
+		return;
+	}
+
+	for (const TSubclassOf<UWarriorGameplayAbility>& Ability : InAbilitiesToGive)
+	{
+		if (!Ability) continue;
+
+		FGameplayAbilitySpec AbilitySpec(Ability);
+		AbilitySpec.SourceObject = InWarriorASCToGive->GetAvatarActor();
+		AbilitySpec.Level = ApplyLevel;
+
+		InWarriorASCToGive->GiveAbility(AbilitySpec);
+	}
+}
