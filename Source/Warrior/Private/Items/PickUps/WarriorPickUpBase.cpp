30e7e35bb2001fc6118ca5add17aeff640ac5101
Gall-oDrone <piehavok@hotmail.com>
2025-02-23 17:13:06 -0600

    Stone Class

---
+++ Source/Warrior/Private/Items/PickUps/WarriorPickUpBase.cpp
@@ -0,0 +1,21 @@
+// Diego Gallo All Rights Reserved
+
+
+#include "Items/PickUps/WarriorPickUpBase.h"
+#include "Components/SphereComponent.h"
+
+// Sets default values
+AWarriorPickUpBase::AWarriorPickUpBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	PickUpCollisionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("PickUpCollisionSphere"));
+	SetRootComponent(PickUpCollisionSphere);
+	PickUpCollisionSphere->InitSphereRadius(50.f);
+	PickUpCollisionSphere->OnComponentBeginOverlap.AddUniqueDynamic(this,&ThisClass::OnPickUpCollisionSphereBeginOverlap);
+}
+
+void AWarriorPickUpBase::OnPickUpCollisionSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+
+}
