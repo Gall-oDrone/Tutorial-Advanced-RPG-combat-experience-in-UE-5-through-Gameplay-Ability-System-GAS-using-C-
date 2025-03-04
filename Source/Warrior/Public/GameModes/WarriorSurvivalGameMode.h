f6ac831cd35a7bba2820a7a0358242345d656dd4
Gall-oDrone <piehavok@hotmail.com>
2025-03-04 14:08:16 -0600

    Wave Progression Logic

--- Source/Warrior/Public/GameModes/WarriorSurvivalGameMode.h
+++ Source/Warrior/Public/GameModes/WarriorSurvivalGameMode.h
@@ -63,6 +63,7 @@ protected:
 
 private:
 	void SetCurrentSurvivalGameModeState(EWarriorSurvivalGameModeState InState);
+	bool HasFinishedAllWaves() const;
 
 	UPROPERTY()
 	EWarriorSurvivalGameModeState CurrentSurvivalGameModeState;
@@ -72,4 +73,22 @@ private:
 
 	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="WaveDefinition", meta = (AllowPrivateAccess = "true"))
 	UDataTable* EnemyWaveSpawnerDataTable;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "WaveDefinition", meta = (AllowPrivateAccess = "true"))
+	int32 TotalWavesToSpawn;
+
+	UPROPERTY()
+	int32 CurrentWaveCount = 1;
+
+	UPROPERTY()
+	float TimePassedSinceStart = 0.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "WaveDefinition", meta = (AllowPrivateAccess = "true"))
+	float SpawnNewWaveWaitTime = 5.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "WaveDefinition", meta = (AllowPrivateAccess = "true"))
+	float SpawnEnemiesDelayTime = 2.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "WaveDefinition", meta = (AllowPrivateAccess = "true"))
+	float WaveCompletedWaitTime = 5.f;
 };
