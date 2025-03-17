ebdf8a5a0cea55284aa6bf4a3261005fbe05248d
Gall-oDrone <piehavok@hotmail.com>
2025-03-17 14:01:24 -0600

    Get Game Level by Tag

--- Source/Warrior/Public/WarriorGameInstance.h
+++ Source/Warrior/Public/WarriorGameInstance.h
@@ -4,8 +4,25 @@
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
+#include "GameplayTagContainer.h"
 #include "WarriorGameInstance.generated.h"
 
+USTRUCT(BlueprintType)
+struct FWarriorGameLevelSet
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, meta = (Categories = "GameData.Level"))
+	FGameplayTag LevelTag;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSoftObjectPointer<UWorld> Level;
+
+	bool IsValid() const;
+	{
+		return LevelTag.IsValid() && !Level.IsNull();
+	}
+};
 /**
  * 
  */
@@ -14,4 +31,11 @@ class WARRIOR_API UWarriorGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
 	
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TArray<FWarriorGameLevelSet> GameLevelSets;
+
+public:
+	UFUNCTION(BlueprintCallable, meta = (GameplayTagFilter="GameData.Level"))
+	TSoftObjectPtr<UWorld> GetGameLevelByTag(FGameplayTag InTag);
 };
